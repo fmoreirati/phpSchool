c2545f077524d26e610529a7c77fb5a0410888a2
fMoreiraTI <fabianomoreira.ti@gmail.com>
2020-08-24 22:07:51 -0300

    first commit

---
+++ pages/page-footer.php
@@ -0,0 +1,3 @@
+<footer class="text-center bg-dark text-light py-3 fixed-bottom">
+    <h5> Feito por Fabiano Moreira </h5>
+</footer>
---
+++ pages/page-header.php
@@ -0,0 +1,3 @@
+<header class="p-2">
+    <img src="img/phpschool.png" alt="Logotipo da PHP School">
+</header>
