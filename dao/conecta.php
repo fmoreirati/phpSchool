f9a26dbd0b41a6a81cc433cfc9c522327ddf02ee
fMoreiraTI <fabianomoreira.ti@gmail.com>
2020-10-21 13:28:24 -0300

    202010121

--- dao/conecta.php
+++ dao/conecta.php
@@ -6,7 +6,7 @@ class DAO
     {
         $pdo = null;
         try {
-            $pdo = new PDO("mysql:host=". HOST .";dbname=". DB , USER, PASS);
+            $pdo = new PDO("mysql:host=". HOST .";port=". PORT .";dbname=". DB , USER, PASS);
             //$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
